17f47b03f1c0e89809ec93780230d269d507015b
Tom Whittock <tom.whittock@disguise.one>
2025-03-20 23:12:23 +0000

    Fix CSS

--- vite.config.js
+++ vite.config.js
@@ -1,10 +1,11 @@
 import { defineConfig } from 'vite'
 import { resolve } from 'path'
 import vue from '@vitejs/plugin-vue'
+import { libInjectCss } from 'vite-plugin-lib-inject-css';
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [vue()],
+  plugins: [vue(), libInjectCss()],
   build: {
     lib: {
       entry: resolve(__dirname, 'src/index.js'),
